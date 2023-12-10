c7c913da1d75a155f0a14d978816a9b220bc0eca
DESKTOP-SHFSN5N\user <m.amirnaqiuddin@gmail.com>
2023-12-11 00:00:24 +0800

    finish calculator

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,85 @@ import "./App.css";
 
 function App() {
 
+  //state
+  const [answer, setAnswer] =  useState("");
+  const [expression, setExpression] = useState("");
+
+  const et = expression.trim();  //Remove spaces 
+
   const buttonPress = (symbol: string) => {
-    console.log(symbol);
+    
+    const isOperator = (symbol: string) => {
+      return /[*/+-]/.test(symbol);  //return true if the symbol is operator
+    };
+
+    const calculate = (symbol: string) => {
+      //if last char is an operator, do nothing
+      if (isOperator(et.charAt(et.length - 1))) return;
+
+      // clean the expression so that two operator in a row uises the last operator
+      //5 * - + 5 = 10
+      const parts = et.split(" ");
+      const newParts = [];
+
+
+      //go through parts backwards
+      for (let i = parts.length - 1; i >= 0; i--) {
+        if(["*", "/", "+"].includes(parts[i]) && isOperator(parts[i - 1])){
+          newParts.unshift(parts[i]);
+          let j = 0;
+          let k = i - 1;
+          while (isOperator(parts[k])){
+            k--;
+            j++;
+          }
+          i -= j;
+        } else {
+          newParts.unshift(parts[i]);
+        }
+      }
+
+      const newExpression = newParts.join(" ");
+      if(isOperator(newExpression.charAt(0))) {
+        setAnswer(eval(answer + newExpression) as string)
+      } else {
+        setAnswer(eval(newExpression) as string)
+      }
+      setExpression("");
+    };
+
+
+    if (symbol === "clear"){
+      setAnswer("");
+      setExpression("0");
+    }else if (symbol === "negative"){
+      if(answer === "") return;
+      setAnswer(answer.toString().charAt(0) === "-" ? answer.slice(1) : "-" + answer);
+    }else if (symbol === "percent"){
+      if(answer === "") return;
+      setAnswer((parseFloat(answer)/100).toString());
+    }else if (isOperator(symbol)){
+      setExpression(et + " " + symbol + " ");
+    }else if (symbol === "="){
+      calculate();
+    }else if (symbol === "0"){
+      if(expression.charAt(0) !== "0"){
+        setExpression(expression + symbol);
+      }
+    }else if (symbol === "."){
+      //split by operators and get the last number
+      const lastNumber = expression.split(/[-+/*]/g).pop();
+      // if last number already has a decimal, dont add another
+      if(lastNumber?.includes(".")) return;
+      setExpression(expression + symbol);
+    }else{
+      if(expression.charAt(0) === "0"){
+        setExpression(expression.slice(1)+symbol);
+      }else{
+        setExpression(expression + symbol)
+      }
+    }
+
   };
   
   return (
@@ -13,14 +90,14 @@ function App() {
         <h1>Calculator App</h1>
         <div id="calculator">
 
-          <div id="display">
-            <div id="answer"></div>
-            <div id="expression"></div>
+          <div id="display" style={{textAlign:"right"}}>
+            <div id="answer">{answer}</div>
+            <div id="expression">{expression}</div>
           </div>
           
           <button id="clear" className="light-gray" onClick={() => buttonPress("clear")} >C</button>
           <button id="negative" className="light-gray" onClick={() => buttonPress("negative")} >+/-</button>
-          <button id="percentage" className="light-gray" onClick={() => buttonPress("percentage")} >%</button>
+          <button id="percent" className="light-gray" onClick={() => buttonPress("percent")} >%</button>
           <button id="divide" className="yellow" onClick={() => buttonPress("/")} >/</button>
           <button id="seven" className="dark-gray" onClick={() => buttonPress("7")} >7</button>
           <button id="eight" className="dark-gray" onClick={() => buttonPress("8")} >8</button>
